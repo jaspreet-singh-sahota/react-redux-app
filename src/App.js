4e17d79da92343876febe061919832eea60d20cf
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-09 14:03:22 +0530

    Add cake component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import cakeContainer from './components/cakeContainer';
+
+function App() {
+  return (
+    <div className="App">
+     <cakeContainer/>
+    </div>
+  );
+}
+
+export default App;
